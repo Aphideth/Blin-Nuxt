00f33ec1f5e5c31da7e5b27627b6f5d709deacaa
Aphideth <aphideth.dev@gmail.com>
2022-02-28 22:30:47 +0100

    get data

---
+++ pages/cuvees.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-container>
+      <v-flex xs12 pb-3>
+        <h1>Nos Cuvées</h1>
+      </v-flex>
+      <v-flex v-for="(article, index) in articles" :key="index">
+        <v-card class="mx-auto" max-width="350">
+          <img :src="article.image" width="200px" />
+          <!-- <img :src="article.composition" width="200px" /> -->
+          <v-card-title> {{ article.name }} </v-card-title>
+          <v-divider></v-divider>
+          <v-card-text>
+            {{ article.description }}
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NosCuvees',
+  async asyncData({ app }) {
+    const { articles } = await app.$axios.$get(
+      'https://gist.githubusercontent.com/Aphideth/82247cbaf15f1fcef3bde2ad5b4fb148/raw/d394d6e51b7f35707abf88e6b695dee4419c5473/champagne.json'
+    )
+    return { articles }
+  },
+}
+</script>
