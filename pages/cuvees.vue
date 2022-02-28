deb5a09173a679c6669725e9e602bf83c9f43361
Aphideth <aphideth.dev@gmail.com>
2022-02-28 23:44:34 +0100

    carousel test

--- pages/cuvees.vue
+++ pages/cuvees.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
+    <v-carousel v-model="model">
+      <v-carousel-item v-for="(article, i) in articles" :key="i">
+        <v-sheet height="100%" tile>
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="text-h2">
+              <h5 align="center">{{ article.name }}</h5>
+              <div align="center">
+                <img :src="article.image" width="250px" align="center" />
+              </div>
+              <p class="text-description">{{ article.description }}</p>
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+    <v-spacer></v-spacer>
     <v-container>
       <v-flex xs12 pb-3>
         <h1>Nos Cuvées</h1>
       </v-flex>
       <v-flex v-for="(article, index) in articles" :key="index">
-        <v-card class="mx-auto" max-width="350">
+        <v-card class="mx-auto" max-width="350" pb-3>
           <img :src="article.image" width="200px" />
           <!-- <img :src="article.composition" width="200px" /> -->
           <v-card-title> {{ article.name }} </v-card-title>
@@ -30,3 +46,9 @@ export default {
   },
 }
 </script>
+
+<style>
+.text-description {
+  font-size: 18px;
+}
+</style>
